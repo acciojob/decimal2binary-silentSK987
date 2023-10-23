c9d3fb008e0beeb87b0b4f2c89fe90cc712c1e10
Pravin Bansod <136956809+silentSK987@users.noreply.github.com>
2023-10-23 16:56:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function decimalToBinary(num) {
   //Write you code here
+	let pv=1;
+	let ans=0;
+	while(num>0){
+		let rem=num%2;
+		num=num/2;
+		ans=ans+rem*pv;
+		pv*=10;
+	}
+	return ans;
+	
   
 }
 
